fb092a0a769af0aa24bcfb48b232fa1b00cfb2ae
Thi <dinhanhthi@gmail.com>
2024-06-29 22:55:31 +0200

    wip: simple test with paste innerHtml

--- src/contentScript/components/RewriteEditor.tsx
+++ src/contentScript/components/RewriteEditor.tsx
@@ -30,15 +30,16 @@ export default function RewriteEditor(props: RewriteEditorProps) {
     // document.execCommand('copy')
     // window.getSelection()?.removeAllRanges()
 
+
     const input = editorRef.current.innerHTML;
-    const inputBlob = new Blob([input], { type: 'text/html' });
+    const inputBlob = new Blob([input], { type: 'text/html' })
     await navigator.clipboard.write([
       new ClipboardItem({
-        'text/html': inputBlob,
-      }),
-    ]);
+        'text/html': inputBlob
+      })
+    ])
 
-    /* ###Thi */ console.log(`👉👉👉 copied!`);
+    /* ###Thi */ console.log(`👉👉👉 copied!`)
   }
 
   return (
@@ -46,6 +47,7 @@ export default function RewriteEditor(props: RewriteEditorProps) {
       <PopoverTrigger></PopoverTrigger>
       <PopoverContent
         asChild
+        side="top"
         container={document.querySelector('.rewrite-overlay')}
         onPointerDownOutside={() => props.setShowRewriteEditor(false)}
       >
@@ -67,8 +69,11 @@ export default function RewriteEditor(props: RewriteEditorProps) {
                     className="h-full w-full text-[15px] cursor-text focus:border-none focus:outline-none whitespace-pre-wrap"
                     dangerouslySetInnerHTML={{
                       // __html:
-                      //   '<p><span style="font-weight:600;background:rgba(251, 243, 219, 1)" data-token-index="0" class="notion-enable-hover">Mistral and Claude</span><span style="background:rgba(251, 243, 219, 1)" data-token-index="1" class="notion-enable-hover"> don’t accept </span><span style="font-family:&quot;SFMono-Regular&quot;, Menlo, Consolas, &quot;PT Mono&quot;, &quot;Liberation Mono&quot;, Courier, monospace;line-height:normal;background:rgba(251, 243, 219, 1);color:#EB5757;border-radius:4px;font-size:85%;padding:0.2em 0.4em" data-token-index="2" spellcheck="false" class="notion-enable-hover">null</span><span style="background:rgba(251, 243, 219, 1)" data-token-index="3" class="notion-enable-hover"> in the request body</span>. OpenAI allows that. In OpenAI request, if you input <span style="font-family:&quot;SFMono-Regular&quot;, Menlo, Consolas, &quot;PT Mono&quot;, &quot;Liberation Mono&quot;, Courier, monospace;line-height:normal;background:rgba(135,131,120,.15);color:#EB5757;border-radius:4px;font-size:85%;padding:0.2em 0.4em" data-token-index="5" spellcheck="false" class="notion-enable-hover">null</span>, the default value for that property will be used. However, there will be an error if you put <span style="font-family:&quot;SFMono-Regular&quot;, Menlo, Consolas, &quot;PT Mono&quot;, &quot;Liberation Mono&quot;, Courier, monospace;line-height:normal;background:rgba(135,131,120,.15);color:#EB5757;border-radius:4px;font-size:85%;padding:0.2em 0.4em" data-token-index="7" spellcheck="false" class="notion-enable-hover">null</span> in the request body of Mistral.<!-- notionvc: 364ad7b0-fef0-49b1-afe6-51e3056c3e3f --><br></p>'
-                      __html: 'Thu nghiem <code>null</code> xem <b>the nao</b> nhe!'
+                      // '<span style="font-weight:600;background:rgba(251, 243, 219, 1)" data-token-index="0" class="notion-enable-hover">Mistral and Claude</span><span style="background:rgba(251, 243, 219, 1)" data-token-index="1" class="notion-enable-hover"> don’t accept </span><span style="font-family:&quot;SFMono-Regular&quot;, Menlo, Consolas, &quot;PT Mono&quot;, &quot;Liberation Mono&quot;, Courier, monospace;line-height:normal;background:rgba(251, 243, 219, 1);color:#EB5757;border-radius:4px;font-size:85%;padding:0.2em 0.4em" data-token-index="2" spellcheck="false" class="notion-enable-hover">null</span><span style="background:rgba(251, 243, 219, 1)" data-token-index="3" class="notion-enable-hover"> in the request body</span>. OpenAI allows that. In OpenAI request, if you input <span style="font-family:&quot;SFMono-Regular&quot;, Menlo, Consolas, &quot;PT Mono&quot;, &quot;Liberation Mono&quot;, Courier, monospace;line-height:normal;background:rgba(135,131,120,.15);color:#EB5757;border-radius:4px;font-size:85%;padding:0.2em 0.4em" data-token-index="5" spellcheck="false" class="notion-enable-hover">null</span>, the default value for that property will be used. However, there will be an error if you put <span style="font-family:&quot;SFMono-Regular&quot;, Menlo, Consolas, &quot;PT Mono&quot;, &quot;Liberation Mono&quot;, Courier, monospace;line-height:normal;background:rgba(135,131,120,.15);color:#EB5757;border-radius:4px;font-size:85%;padding:0.2em 0.4em" data-token-index="7" spellcheck="false" class="notion-enable-hover">null</span> in the request body of Mistral.'
+                      // __html: 'Thu nghiem <code>null</code> xem <b>the nao</b> nhe <s>bi cat</s> con day la <mark>highlight</mark>!'
+                      // __html:
+                      //   'w thu <span style="color:rgba(193, 76, 138, 1);fill:rgba(193, 76, 138, 1)">nge </span>to mobile use, try to thu tstrap'
+                      __html: '<span style="font-weight: 500;"></span>w thu to mobile use, try to thu tstrap'
                     }}
                   ></div>
                 </div>
