a3509f6f728600c776f73e1b314c9dac855165c5
Thi <thi@ideta.io>
2024-07-02 00:12:00 +0200

    feat: handle clicking outside rewriteEditor

--- src/contentScript/components/RewriteEditor.tsx
+++ src/contentScript/components/RewriteEditor.tsx
@@ -43,54 +43,73 @@ export default function RewriteEditor(props: RewriteEditorProps) {
     editors.forEach(editor => editor.remove())
   }
 
+  const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault()
+    e.stopPropagation()
+    /* ###Thi */ console.log(`👉👉👉 handleOverlayClick clicked`)
+    removeAllRewriteEditors()
+  }
+
+  const handleEditorClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault()
+    e.stopPropagation()
+    /* ###Thi */ console.log(`👉👉👉 handleEditorClick clicked`)
+  }
+
   return (
-    <div
-      onClick={e => e.stopPropagation()}
-      className={cn(
-        'h-[200px] w-full !p-0 border-none outline-none bg-transparent',
-        props.className
-      )}
-    >
-      <div className="relative w-full h-full">
-        {/* Main editor */}
-        <div className="z-30 h-full">
-          <div className="h-full w-full rounded-[6px] bg-white p-[0.75em] pb-[1.25em] notion-box-shadow isolate">
-            <div className="overflow-y-auto dat-scrollbar dat-scrollbar-small">
-              <div
-                ref={editorRef}
-                contentEditable={true}
-                suppressContentEditableWarning={true}
-                className="h-full w-full text-[15px] cursor-text focus:border-none focus:outline-none whitespace-pre-wrap"
-                dangerouslySetInnerHTML={{
-                  // __html: 'Thu nghiem <code>null</code> xem <b>the nao</b> nhe <s>bi cat</s> con day la <mark>highlight</mark>!'
-                  // __html:
-                  //   'w thu <span style="color:rgba(193, 76, 138, 1);fill:rgba(193, 76, 138, 1)">nge </span>to mobile use, try to thu tstrap'
-                  __html:
-                    '<span style="font-weight: 500;"></span>w thu to mobile use, try to thu tstrap'
-                }}
-              ></div>
+    <>
+      <div
+        onClick={handleOverlayClick}
+        className="fixed inset-0 z-10 block opacity-0 isolate"
+      ></div>
+      <div
+        onClick={handleEditorClick}
+        className={cn(
+          'h-[200px] w-full !p-0 border-none outline-none bg-transparent z-50 isolate relative',
+          props.className
+        )}
+      >
+        <div className="relative w-full h-full">
+          {/* Main editor */}
+          <div className="z-30 h-full">
+            <div className="h-full w-full rounded-[6px] bg-white p-[0.75em] pb-[1.25em] notion-box-shadow isolate">
+              <div className="overflow-y-auto dat-scrollbar dat-scrollbar-small">
+                <div
+                  ref={editorRef}
+                  contentEditable={true}
+                  suppressContentEditableWarning={true}
+                  className="h-full w-full text-[15px] cursor-text focus:border-none focus:outline-none whitespace-pre-wrap"
+                  dangerouslySetInnerHTML={{
+                    // __html: 'Thu nghiem <code>null</code> xem <b>the nao</b> nhe <s>bi cat</s> con day la <mark>highlight</mark>!'
+                    // __html:
+                    //   'w thu <span style="color:rgba(193, 76, 138, 1);fill:rgba(193, 76, 138, 1)">nge </span>to mobile use, try to thu tstrap'
+                    __html:
+                      '<span style="font-weight: 500;"></span>w thu to mobile use, try to thu tstrap'
+                  }}
+                ></div>
+              </div>
+            </div>
+          </div>
+
+          <button
+            onClick={handleNothing}
+            className="absolute top-0 left-0 z-50 w-full h-full"
+          ></button>
+
+          {/* Controls */}
+          {/* -15px = (32 height - 2 border) / 2 */}
+          <div className="absolute bottom-[-15px] right-0 z-50 flex h-[32px] gap-4 pr-4">
+            <div className="rounded-[1em] w-fit bg-white overflow-hidden h-[32px] inline-flex items-center justify-center text-[0.95em] whitespace-nowrap">
+              <button
+                onClick={useThisContent}
+                className="h-full px-5 text-white transition-all bg-green-700 w-fit hover:px-6 _bg-rainbow group"
+              >
+                Use this text
+              </button>
             </div>
           </div>
         </div>
-
-        <button
-          onClick={handleNothing}
-          className="absolute top-0 left-0 z-50 w-full h-full"
-        ></button>
-
-        {/* Controls */}
-        {/* -15px = (32 height - 2 border) / 2 */}
-        <div className="absolute bottom-[-15px] right-0 z-50 flex h-[32px] gap-4 pr-4">
-          <div className="rounded-[1em] w-fit bg-white overflow-hidden h-[32px] inline-flex items-center justify-center text-[0.95em] whitespace-nowrap">
-            <button
-              onClick={useThisContent}
-              className="h-full px-5 text-white transition-all bg-green-700 w-fit hover:px-6 _bg-rainbow group"
-            >
-              Use this text
-            </button>
-          </div>
-        </div>
       </div>
-    </div>
+    </>
   )
 }
