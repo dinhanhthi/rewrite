7589d87555a346c159231859510e21e052812732
Thi <thi@ideta.io>
2024-07-01 23:52:00 +0200

    Add tooltip to rewrite btn

--- src/contentScript/components/notion/RewriteBtn.tsx
+++ src/contentScript/components/notion/RewriteBtn.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '../../../components/ui/tooltip'
 import { cn } from '../../../helpers/helpers'
-import { LogoRewriteIcon } from '../../../icons/LogoRewriteIcon'
+import LogoRewriteIcon from '../../../icons/LogoRewriteIcon'
 
 type ReWriteBtnProps = {
   className?: string
@@ -8,14 +14,23 @@ type ReWriteBtnProps = {
 
 export default function RewriteBtn(props: ReWriteBtnProps) {
   return (
-    <button
-      className={cn(
-        props.className,
-        'flex items-center justify-center h-full w-full px-2 rounded-md group-active:scale-95 select-none transition-transform group-hover:bg-[#37352f14] text-green-700 gap-1.5'
-      )}
-    >
-      <LogoRewriteIcon className="w-5 h-5" />
-      <div className="text-[14.5px] font-medium whitespace-nowrap">Rewrite</div>
-    </button>
+    <TooltipProvider delayDuration={100} skipDelayDuration={10} disableHoverableContent={true}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <button
+            className={cn(
+              props.className,
+              'flex items-center justify-center h-full w-full px-2 rounded-md group-active:scale-95 select-none transition-transform group-hover:bg-[#37352f14] text-green-700 gap-1.5'
+            )}
+          >
+            <LogoRewriteIcon className="w-5 h-5" />
+            <div className="text-[14.5px] font-medium whitespace-nowrap">Rewrite</div>
+          </button>
+        </TooltipTrigger>
+        <TooltipContent className='text-[12px] text-white bg-black px-1.5 py-1'>
+          Let AI help you rewrite your text
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
