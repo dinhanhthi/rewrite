ecbaf0d93f5daeeb23c7aa9b37f9c142719ad97a
Thi <dinhanhthi@gmail.com>
2024-07-06 12:51:37 +0200

    feat: add description + tooltip for playground menu
    chore

--- src/playground/App.tsx
+++ src/playground/App.tsx
@@ -5,6 +5,7 @@ import { House } from 'lucide-react'
 import { Link, Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import { Button } from '../components/ui/button'
 import { Toaster } from '../components/ui/toaster'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../components/ui/tooltip'
 import '../styles/global.scss'
 import '../styles/options.scss'
 import '../styles/playground.scss'
@@ -34,23 +35,38 @@ export default function App() {
           )}
         >
           {menus.map(menu => (
-            <Button
-              asChild
-              className={cn('cursor-pointer', {
-                'bg-sky-500 text-white pointer-events-none': location.pathname === menu.path,
-                'bg-white text-slate-800': location.pathname !== menu.path
-              })}
-              key={menu.name}
-              variant="outline"
+            <TooltipProvider
+              delayDuration={100}
+              skipDelayDuration={10}
+              disableHoverableContent={true}
             >
-              <Link
-                className="tracking-wide text-[1em] gap-[0.3em] !py-1 !px-3 h-auto font-normal rounded-3xl"
-                to={menu.path}
-              >
-                {menu.name === 'home' && <House size={20} />}
-                {menu.name}
-              </Link>
-            </Button>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    asChild
+                    className={cn('cursor-pointer', {
+                      'bg-sky-500 text-white hover:bg-sky-500 hover:text-white': location.pathname === menu.path,
+                      'bg-white text-slate-800': location.pathname !== menu.path
+                    })}
+                    key={menu.name}
+                    variant="outline"
+                  >
+                    <Link
+                      className="tracking-wide text-[1em] gap-[0.3em] !py-1 !px-3 h-auto font-normal rounded-3xl"
+                      to={menu.path}
+                    >
+                      {menu.name === 'home' && <House size={20} />}
+                      {menu.name}
+                    </Link>
+                  </Button>
+                </TooltipTrigger>
+                {menu.description && (
+                  <TooltipContent className="text-[12px] text-white bg-black px-1.5 py-1">
+                    {menu.description}
+                  </TooltipContent>
+                )}
+              </Tooltip>
+            </TooltipProvider>
           ))}
         </div>
         <div className="flex flex-col items-center justify-center flex-auto gap-16">
@@ -78,7 +94,13 @@ export default function App() {
   )
 }
 
-const menus = [
+const menus: {
+  name: string
+  path: string
+  description?: string
+  component: React.FC<any>
+  props?: any
+}[] = [
   {
     name: 'home',
     path: '/',
@@ -94,12 +116,14 @@ const menus = [
   {
     name: 'notionMenu',
     path: '/notionMenu',
+    description: 'When we select text in Notion.',
     component: NotionMenuBar,
     props: {}
   },
   {
     name: 'notionPopover',
     path: '/notionPopover',
+    description: 'When we select blocks or click "..." on the menu.',
     component: NotionPopoverMenu,
     props: {}
   },
@@ -112,6 +136,7 @@ const menus = [
   {
     name: 'CancelDialog',
     path: '/CancelDialog',
+    description: 'When we click outside the editor.',
     component: CancelDialogPlayground,
     props: {}
   },
