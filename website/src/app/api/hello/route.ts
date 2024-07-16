1b393e1ae52d3c40979bbac449fcfbb455875185
Thi <dinhanhthi@gmail.com>
2024-07-16 23:23:41 +0200

    fix: website build

--- website/src/app/api/hello/route.ts
+++ website/src/app/api/hello/route.ts
@@ -1,6 +1,6 @@
 // https://nextjs.org/docs/app/api-reference/file-conventions/route
 
-export async function GET(request: Request) {
+export async function GET(_request: Request) {
   return new Response(JSON.stringify({ name: 'Anh Thi Dinh' }), {
     headers: { 'Content-Type': 'application/json' }
   })
